37031cd641708e8e8663f3af0b8bf8391368e229
Brian Hixson-Simeral <brian.hixsonsimeral@gmail.com>
2015-10-14 10:57:29 -0500

    Ran the code through JSHint to fix any errors.  Started work on the combat encounter logic.  Created a helper function for dice rolls.

--- RPGCombat.js
+++ RPGCombat.js
@@ -3,3 +3,14 @@ Logic for combat
 created by Brian Hixson-Simeral 10/12/2015
 */
 
+function initiativeRoll(initiative){
+	return Math.random()*initiative;
+};
+var playerTurn = initiativeRoll(playerCharacter.initiative) > initiativeRoll(theBigBad.initiative);
+
+
+function combatEncounter (){
+	while(moveCount < 1){
+
+	}
+}
--- RPGTrial.js
+++ RPGTrial.js
@@ -1,3 +1,4 @@
+
 /*
 RPGTrial
 
@@ -24,27 +25,28 @@ function createWarrior(characterName, str, def, mag, res, acc, dod){
 		"wResistance" : 0,
 		"wAccuracy" : 0,
 		"wDodge" : 0,
-		"Experience" : 0
+		"Initiative" : 50,
+		"Experience" : 0,
 
 		totStr : function() {
 			return this.Strength + this.wStrength;
-		}
+		},
 		totDef : function() {
 			return this.Defence + this.wDefence;
-		}
+		},
 		totMag : function() {
 			return this.Magic + this.wMagic;
-		}
+		},
 		totRes : function() {
 			return this.Resistance + this.wResistance;
-		}
+		},
 		totAcc : function() {
 			return this.Accuracy + this.wAccuracy;
-		}
+		},
 		totDod : function() {
 			return this.Dodge + this.wDodge;
 		}
-	}
+	};
 }
 
 function createRouge(characterName, str, def, mag, res, acc, dod){
@@ -64,27 +66,28 @@ function createRouge(characterName, str, def, mag, res, acc, dod){
 		"wResistance" : 0,
 		"wAccuracy" : 0,
 		"wDodge" : 0,
-		"Experience" : 0
+		"Initiative" : 60,
+		"Experience" : 0,
 
 		totStr : function() {
 			return this.Strength + this.wStrength;
-		}
+		},
 		totDef : function() {
 			return this.Defence + this.wDefence;
-		}
+		},
 		totMag : function() {
 			return this.Magic + this.wMagic;
-		}
+		},
 		totRes : function() {
 			return this.Resistance + this.wResistance;
-		}
+		},
 		totAcc : function() {
 			return this.Accuracy + this.wAccuracy;
-		}
+		},
 		totDod : function() {
 			return this.Dodge + this.wDodge;
 		}
-	}
+	};
 }
 
 function createWizard(characterName, str, def, mag, res, acc, dod){
@@ -104,33 +107,34 @@ function createWizard(characterName, str, def, mag, res, acc, dod){
 		"wResistance" : 0,
 		"wAccuracy" : 0,
 		"wDodge" : 0,
-		"Experience" : 0
+		"Initiative" : 65,
+		"Experience" : 0,
 
 		totStr : function() {
 			return this.Strength + this.wStrength;
-		}
+		},
 		totDef : function() {
 			return this.Defence + this.wDefence;
-		}
+		},
 		totMag : function() {
 			return this.Magic + this.wMagic;
-		}
+		},
 		totRes : function() {
 			return this.Resistance + this.wResistance;
-		}
+		},
 		totAcc : function() {
 			return this.Accuracy + this.wAccuracy;
-		}
+		},
 		totDod : function() {
 			return this.Dodge + this.wDodge;
 		}
-	}
+	};
 }
 
 //create all monsters and store their stats
 //Name is the ID of the character
 //Every other state is entirely gameplay based
-function createCharacter(characterName, hp, str, def, mag, res, acc, dod, exp){
+function createCharacter(characterName, hp, str, def, mag, res, acc, dod, exp, init){
 	var theBigBad = {
 		"Name" : characterName,
 		"Health" : hp,
@@ -147,27 +151,28 @@ function createCharacter(characterName, hp, str, def, mag, res, acc, dod, exp){
 		"wResistance" : 0,
 		"wAccuracy" : 0,
 		"wDodge" : 0,
-		"Experience" : exp
+		"Initiative" : init,
+		"Experience" : exp,
 
 		totStr : function() {
 			return this.Strength + this.wStrength;
-		}
+		},
 		totDef : function() {
 			return this.Defence + this.wDefence;
-		}
+		},
 		totMag : function() {
 			return this.Magic + this.wMagic;
-		}
+		},
 		totRes : function() {
 			return this.Resistance + this.wResistance;
-		}
+		},
 		totAcc : function() {
 			return this.Accuracy + this.wAccuracy;
-		}
+		},
 		totDod : function() {
 			return this.Dodge + this.wDodge;
 		}
-	}
+	};
 }
 
 
@@ -177,28 +182,34 @@ function createCharacter(characterName, hp, str, def, mag, res, acc, dod, exp){
 //The functions to create the monster's stats
 //These will be called when the player encounters a monster
 //The monster that the player fights is determined by Math.random()
+var slimeCount = 0;
 function createSlime(){
-	createCharacter("Slime" + slimeCount, 7, 2, 4, 0, 2, 5, 5, 5);
+	createCharacter("Slime" + slimeCount, 7, 2, 4, 0, 2, 5, 5, 5, 30);
 	slimeCount++;
 }
 
+var boarCount = 0;
 function createBoar(){
-	createCharacter("Boar" + boarCount, 12, 5, 5, 0, 1, 6, 4, 9);
+	createCharacter("Boar" + boarCount, 12, 5, 5, 0, 1, 6, 4, 9, 40);
 	boarCount++;
 }
 
+var koboldMageCount = 0;
 function createKoboldMage(){
-	createCharacter("Kobold Mage" + kobolMageCount, 23, 6, 7, 12, 10, 10, 6, 25);
-	kobolMageCount++;
+	createCharacter("Kobold Mage" + koboldMageCount, 23, 6, 7, 12, 10, 10, 6, 25, 40);
+	koboldMageCount++;
 }
 
+var dragonCount = 0;
 function createDragon(){
-	createCharacter("Dragon" + dragonCount, 102, 24, 30, 18, 23, 12, 16, 250);
+	createCharacter("Dragon" + dragonCount, 102, 24, 30, 18, 23, 12, 16, 250, 60);
 	dragonCount++;
 }
 
 
-
+function randomBound(lowerBound, randModifier){
+  return Math.ceil(Math.random()*randModifier)+lowerBound;
+}
 
 
 
@@ -207,11 +218,12 @@ function attack (unit1, unit2){
 	var hitFloor = 50 + unit2.totDod() - unit1.totAcc();
 	var hitRoll = Math.ceil(Math.random()*100);
 	var critChance = 100 - unit1.totAcc();
+  var damageDealt;
 
 	//crit
 	if(hitRoll > critChance){
 		//print crit message
-		var damageDealt = Math.floor(2.5 * unit1.totStr()) - unit2.totDef();
+		damageDealt = Math.floor(2.5 * unit1.totStr()) - unit2.totDef();
 		if(damageDealt > 0){
 			//print damage message
 			unit2.Health -= damageDealt;
@@ -224,7 +236,7 @@ function attack (unit1, unit2){
 	//hit
 	else if(hitRoll > hitFloor){
 		//print hit message
-		var damageDealt = unit1.totStr() - unit2.totDef();
+		damageDealt = unit1.totStr() - unit2.totDef();
 		if(damageDealt > 0){
 			//print damage message
 			unit2.Health -= damageDealt;
@@ -244,11 +256,12 @@ function cast (unit1, unit2){
 	var hitFloor = 50 + unit2.totDod() - unit1.totAcc();
 	var hitRoll = Math.ceil(Math.random()*100);
 	var critChance = 100 - unit1.totAcc();
+  var damageDealt;
 
 	//crit
 	if(hitRoll > critChance){
 		//print crit message
-		var damageDealt = Math.floor(2.5 * unit1.totMag()) - unit2.totRes();
+		damageDealt = Math.floor(2.5 * unit1.totMag()) - unit2.totRes();
 		if(damageDealt > 0){
 			//print damage message
 			unit2.Health -= damageDealt;
@@ -261,7 +274,7 @@ function cast (unit1, unit2){
 	//hit
 	else if(hitRoll > hitFloor){
 		//print hit message
-		var damageDealt = unit1.totMag() - unit2.totRes();
+		damageDealt = unit1.totMag() - unit2.totRes();
 		if(damageDealt > 0){
 			//print damage message
 			unit2.Health -= damageDealt;
@@ -284,22 +297,28 @@ function createWeapon(){
 	var statArray = [0, 0, 0, 0, 0, 0];
 	var weaponType = Math.ceil(Math.random()*100);
 	var weaponClass = Math.ceil(Math.random()*100);
+  var bonusStrength;
+  var bonusDefence;
+  var bonusMagic;
+  var bonusResistance;
+  var bonusAccuracy;
+  var bonusDodge;
 
 	//print the weapon type and the weapon class
 	if(weaponType < 34){
 		//Sword and shield
 		if(weaponClass < 60){
-			var bonusStrength = Math.ceil(Math.random() * 7) + 3;
-			var bonusDefence = Math.ceil(Math.random() * 7) + 3;
+			bonusStrength = randomBound(3, 7);
+			bonusDefence = randomBound(3, 7);
 
 		}
 		else if(weaponClass < 90){
-			var bonusStrength = Math.ceil(Math.random() * 14) + 7;
-			var bonusDefence = Math.ceil(Math.random() * 14) + 7;
+			bonusStrength = randomBound(7, 14);
+			bonusDefence = randomBound(7, 14);
 		}
 		else{
-			var bonusStrength = Math.ceil(Math.random() * 20) + 15;
-			var bonusDefence = Math.ceil(Math.random() * 20) + 15;
+			bonusStrength = randomBound(15, 20);
+			bonusDefence = randomBound(15, 20);
 
 		}
 	}
@@ -307,63 +326,63 @@ function createWeapon(){
 	else if(weaponType < 67){
 		//Tome and staff
 		if(weaponClass < 60){
-			var bonusMagic = Math.ceil(Math.random() * 7) + 3;
-			var bonusResistance = Math.ceil(Math.random() * 7) + 3;
+			bonusMagic = randomBound(3, 7);
+			bonusResistance = randomBound(3, 7);
 		}
 		else if(weaponClass < 90){
-			var bonusMagic = Math.ceil(Math.random() * 14) + 7;
-			var bonusResistance = Math.ceil(Math.random() * 14) + 7;
+			bonusMagic = randomBound(7, 14);
+			bonusResistance = randomBound(7, 14);
 		}
 		else{
-			var bonusMagic = Math.ceil(Math.random() * 20) + 15;
-			var bonusResistance = Math.ceil(Math.random() * 20) + 15;
+			bonusMagic = randomBound(15, 20);
+			bonusResistance = randomBound(15, 20);
 		}
 	}
 
 	else if(weaponType < 100){
 		//Cloak and dagger
 		if(weaponClass < 60){
-			var bonusAccuracy = Math.ceil(Math.random() * 7) + 3;
-			var bonusDodge = Math.ceil(Math.random() * 7) + 3;
+			bonusAccuracy = randomBound(3, 7);
+			bonusDodge = randomBound(3, 7);
 		}
 		else if(weaponClass < 90){
-			var bonusAccuracy = Math.ceil(Math.random() * 14) + 7;
-			var bonusDodge = Math.ceil(Math.random() * 14) + 7;
+			bonusAccuracy = randomBound(7, 14);
+			bonusDodge = randomBound(7, 14);
 		}
 		else{
-			var bonusAccuracy = Math.ceil(Math.random() * 20) + 15;
-			var bonusDodge = Math.ceil(Math.random() * 20) + 15;
+			bonusAccuracy = randomBound(15, 20);
+			bonusDodge = randomBound(15, 20);
 
 		}
 	}
 
-	else if(weaponType = 100){
+	else if(weaponType == 100){
 		//Magic amulet
 		if(weaponClass < 60){
-			var bonusStrength = Math.ceil(Math.random() * 7) + 3;
-			var bonusDefence = Math.ceil(Math.random() * 7) + 3;
-			var bonusMagic = Math.ceil(Math.random() * 7) + 3;
-			var bonusResistance = Math.ceil(Math.random() * 7) + 3;
-			var bonusAccuracy = Math.ceil(Math.random() * 7) + 3;
-			var bonusDodge = Math.ceil(Math.random() * 7) + 3;
+			bonusStrength = randomBound(3, 7);
+			bonusDefence = randomBound(3, 7);
+			bonusMagic = randomBound(3, 7);
+			bonusResistance = randomBound(3, 7);
+			bonusAccuracy = randomBound(3, 7);
+			bonusDodge = randomBound(3, 7);
 
 		}
 		else if(weaponClass < 90){
-			var bonusStrength = Math.ceil(Math.random() * 14) + 7;
-			var bonusDefence = Math.ceil(Math.random() * 14) + 7;
-			var bonusMagic = Math.ceil(Math.random() * 14) + 7;
-			var bonusResistance = Math.ceil(Math.random() * 14) + 7;
-			var bonusAccuracy = Math.ceil(Math.random() * 14) + 7;
-			var bonusDodge = Math.ceil(Math.random() * 14) + 7;
+			bonusStrength = randomBound(7, 14);
+			bonusDefence = randomBound(7, 14);
+			bonusMagic = randomBound(7, 14);
+			bonusResistance = randomBound(7, 14);
+			bonusAccuracy = randomBound(7, 14);
+			bonusDodge = randomBound(7, 14);
 
 		}
 		else{
-			var bonusStrength = Math.ceil(Math.random() * 20) + 15;
-			var bonusDefence = Math.ceil(Math.random() * 20) + 15;
-			var bonusMagic = Math.ceil(Math.random() * 20) + 15;
-			var bonusResistance = Math.ceil(Math.random() * 20) + 15;
-			var bonusAccuracy = Math.ceil(Math.random() * 20) + 15;
-			var bonusDodge = Math.ceil(Math.random() * 20) + 15;
+			bonusStrength = randomBound(15, 20);
+			bonusDefence = randomBound(15, 20);
+			bonusMagic = randomBound(15, 20);
+			bonusResistance = randomBound(15, 20);
+			bonusAccuracy = randomBound(15, 20);
+			bonusDodge = randomBound(15, 20);
 		}
 	}
 
