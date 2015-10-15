a4cdc2e1d7255b206445347a12ea9f40f9f85ff6
Brian Hixson-Simeral <bhixsonsimeral16@csl18-l.cornellcollege.edu>
2015-10-15 10:31:08 -0500

    Worked on the encounter logic, focusing mainly on the AI of the enemy.

--- RPGCombat.js
+++ RPGCombat.js
@@ -3,14 +3,80 @@ Logic for combat
 created by Brian Hixson-Simeral 10/12/2015
 */
 
-function initiativeRoll(initiative){
-	return Math.random()*initiative;
-};
-var playerTurn = initiativeRoll(playerCharacter.initiative) > initiativeRoll(theBigBad.initiative);
+var attackMove = true;
+var caster = false;
+var attacker = false;
+var pDamage = 0;
+var mDamage = 0;
 
+//determines who goes first
+function initiativeRoll(initiative){
+	return (Math.random()*100)+initiative;
+}
+
+function enemyMove(){
+    if (!caster){
+	attackMove = false;
+}
+    else if(!attacker){
+	attackMove = true;
+}
+    else if(mDamage > pDamage){
+	attackMove = false;
+}
+    else if(pDamage > mDamage){
+	attackMove = true;
+}
+    if(attackMove){
+	if(Math.random() < 0.8){
+	    attack(theBigBad, playerCharacter);
+	    pDamage = attack.damageDealt;
+}
+	else{
+	    cast(theBigBad, playerCharacter);
+	    mDamage = cast.damageDealt;
+}
+}
+
+    else if(!attackMove){
+	if(Math.random() < 0.8){
+	    cast(theBigBad, playerCharacter);
+	    mDamage = cast.damageDealt;
+}
+	else{
+	    attack(theBigBad, playerCharacter);
+	    pDamage = attack.damageDealt;
+}
+}
+}
 
 function combatEncounter (){
-	while(moveCount < 1){
+    var enemyRoll = Math.floor(Math.random()*100);
+    if(enemyRoll < 40){
+	createSlime();
+}
+    else if(enemyRoll < 60){
+	createBoar();
+}
+    else if(enemyRoll < 85){
+	createKoboldMage();
+}
+    else{
+	createDragon();
+}
+    attackMove = true;
+		caster = false;
+		attacker = false;
+		pDamage = 0;
+		mDamage = 0;
+    var playerTurn = initiativeRoll(playerCharacter.initiative) > initiativeRoll(theBigBad.initiative);
 
-	}
+    while(theBigBad.Health !== 0 || playerCharacter.Health !== 0){
+    while (playerTurn){
+	
+}
+    if (!playerTurn){
+	enemyMove();
+}
+}
 }
